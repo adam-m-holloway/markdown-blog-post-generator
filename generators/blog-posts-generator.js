cd5d3376b85034ee552865e711c1659e28c832c2
Adam Holloway <adam.holloway21@gmail.com>
2019-10-06 17:46:26 +0100

    add helper comments to generator

--- generators/blog-posts-generator.js
+++ generators/blog-posts-generator.js
@@ -6,23 +6,27 @@ const authors = JSON.parse(fs.readFileSync('./config/authors.json'));
 module.exports = plop => {
   plop.setGenerator('blog post', {
     prompts: [
+      // Question 1
       {
         type: 'input',
         name: 'title',
         message: 'Blog post title?',
         validate: inputRequired('title')
       },
+      // Question 2
       {
         type: 'list',
         name: 'author',
         message: 'The author of blog post?',
         choices: authors.map(author => ({ name: author.id, value: author.id }))
       },
+      // Question 3
       {
         type: 'input',
         name: 'tags',
         message: 'tags? (separate with coma)'
       },
+      // Question 4
       {
         type: 'confirm',
         name: 'draft',
@@ -30,10 +34,10 @@ module.exports = plop => {
       }
     ],
     actions: data => {
-      // Get current date
+      // Get the current date
       data.date = new Date().toISOString().split('T')[0];
 
-      // Parse tags as yaml array
+      // Parse tags as YAML array
       if (data.tags) {
         data.tags = `\ntags:\n  - ${data.tags.split(',').join('\n  - ')}`;
       }
